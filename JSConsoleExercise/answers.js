c0874ac5af8a2121fc91e4b58d8bcc68ce1497bb
johndward01 <johndward01@gmail.com>
2021-02-17 00:47:03 -0600

    initial commit

---
+++ JSConsoleExercise/answers.js
@@ -0,0 +1,22 @@
+/*
+    for(i=0; i<artists.length;i++){
+        if(artists[i].first == "Chaim"){
+            document.querySelector('h1').textContent = 'Chaim! I found you!';
+        }}
+*/
+
+/*
+while(i < artists.length){
+    if(artists[i].first == "Chaim"){
+        document.querySelector('h1').textContent = 'Chaim! I found you!';
+    }
+    i++;
+}
+*/
+
+function myFunction(someArray) {
+    let i = 0;
+    while (i < someArray.length) {
+        console.log(someArray[i]);
+    }
+}
